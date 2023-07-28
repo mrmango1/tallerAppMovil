df3c35e10bda4b3987f976a76edb43984fe06ef8
Anderson Grefa <andersongrefa@gmail.com>
2023-07-27 20:44:42 -0500

    feat: agregando componentes

---
+++ src/components/drawer/CustomDrawer.jsx
@@ -0,0 +1,154 @@
+import React, { useReducer, useRef } from 'react'
+import { useDrawerProgress } from '@react-navigation/drawer'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Animated, { interpolate, useAnimatedStyle, useDerivedValue, withSpring, withTiming } from 'react-native-reanimated'
+import DrawerItemList from './DrawerItemList'
+import { colors, constant } from './constants'
+import Constants from 'expo-constants'
+import Icon, { Icons } from '../Icons'
+
+const ProfileMenu = [
+  { label: 'Mi Perfil', icon: 'person', iconType: Icons.MaterialIcons, screen: 'user' },
+  { label: 'Historial', icon: 'history', iconType: Icons.MaterialIcons, screen: 'history' },
+  { label: 'Cerrar Sesión', icon: 'logout', iconType: Icons.MaterialIcons, screen: 'Login' }
+]
+
+const ProfileItem = ({ label, onPress, type, name, navigation }) => {
+  const navigate = () => {
+    if (onPress === 'Login') {
+      navigation.replace('Login')
+    } else {
+      navigation.navigate(onPress)
+    }
+  }
+  return (
+    <TouchableOpacity
+      onPress={navigate}
+      style={[styles.row, { margin: constant.SPACING / 4 }]}>
+      <Icon type={type} name={name} color={colors.dark} />
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const CustomDrawer = (props) => {
+  const { state, descriptors, navigation } = props
+  const scrollRef = useRef(null)
+  const [show, toggleProfile] = useReducer(s => !s, false)
+
+  const fun = () => {
+    show
+      ? scrollRef.current.scrollTo({
+        y: 100,
+        animated: true
+      })
+      : scrollRef.current.scrollToEnd({
+        y: 100,
+        animated: true
+      })
+    toggleProfile()
+  }
+  const progress = useDerivedValue(() => {
+    return show ? withTiming(1) : withTiming(0)
+  })
+
+  const menuStyles = useAnimatedStyle(() => {
+    const scaleY = interpolate(
+      progress.value,
+      [0, 1],
+      [0, 1]
+    )
+    return {
+      transform: [{ scaleY }]
+    }
+  })
+
+  const drawerProgress = useDrawerProgress()
+
+  const viewStyles = useAnimatedStyle(() => {
+    const translateX = interpolate(
+      drawerProgress.value,
+      [0, 1],
+      [-200, 0]
+    )
+    return {
+      transform: [{ translateX }]
+    }
+  })
+
+  return (
+  <View style={styles.container}>
+    <TouchableOpacity onPress={fun}>
+        <Animated.View style={[styles.marginTop, styles.view, styles.row]}>
+          <Image style={styles.profile} source={require('../../assets/avatar.png')} />
+          <View style={styles.textContainer}>
+            <Text style={styles.headerTitle}>Anderson Grefa</Text>
+            <Text style={styles.text}>Administrador</Text>
+          </View>
+        </Animated.View>
+    </TouchableOpacity>
+    <Animated.ScrollView
+      ref={scrollRef} {...props}
+      showsVerticalScrollIndicator={false}
+      style={[styles.marginVertical, viewStyles]}>
+        {show &&
+          <Animated.View style={[styles.view, styles.marginBottom,
+            { backgroundColor: colors.primary },
+            menuStyles
+          ]}>
+            <Text>andersongrefa@gmail.com</Text>
+            <View style={styles.separator} />
+            {ProfileMenu.map((_, i) => (
+              <ProfileItem key={i}
+                label={_.label}
+                type={_.iconType}
+                name={_.icon}
+                onPress={_.screen}
+                navigation={navigation}
+              />
+            ))}
+          </Animated.View>
+        }
+        <DrawerItemList {...props}/>
+    </Animated.ScrollView>
+  </View>)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  view: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginHorizontal: 8,
+    padding: 16 / 1.5
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  marginTop: {
+    marginTop: Constants.statusBarHeight + 10
+  },
+  marginBottom: {
+    marginBottom: constant.SPACING / 2
+  },
+  marginVertical: {
+    marginVertical: constant.SPACING / 2
+  },
+  profile: {
+    width: 50,
+    height: 50
+  },
+  headerTitle: {
+    fontSize: 16,
+    color: '#123',
+    fontWeight: 'bold'
+  },
+  textContainer: {
+    marginLeft: 16
+  }
+})
+
+export default CustomDrawer
---
+++ src/components/drawer/DrawerItemList.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native'
+import { colors, constant } from './constants'
+import Icon from '../Icons'
+
+const DrawerItem = ({
+  label, onPress, tabBarTestID, type, name, notification,
+  activeItemColor, color
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      testID={tabBarTestID}
+      accessibilityRole="button"
+      style={[styles.drawerItem, { backgroundColor: activeItemColor }]}
+    >
+      <View style={styles.row}>
+        <Icon type={type} name={name} {...{ color }} />
+        <Text style={[styles.label, { color }]}>{label}</Text>
+      </View>
+      {notification > 0 && <View style={[styles.notificationBadge,
+        { backgroundColor: notification > 5 ? colors.important : colors.normal }]}>
+        <Text>{notification}</Text>
+      </View>}
+    </TouchableOpacity>
+  )
+}
+
+const DrawerItemList = ({ state, descriptors, navigation }) => {
+  return (
+    <View style={styles.view}>
+      {state.routes.map((route, index) => {
+        const isFocused = state.index === index
+        const { options } = descriptors[route.key]
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key
+          })
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name)
+          }
+        }
+        const drawerItem = options.item
+        const visible = drawerItem.visible
+        const color = isFocused ? colors.dark : colors.darkGray
+        const activeItemColor = isFocused ? colors.primary : null
+
+        if (visible) {
+          return (
+            <DrawerItem key={index} label={drawerItem.label}
+             tabBarTestID={options.tabBarTestID}
+             onPress={onPress}
+             name={drawerItem.icon}
+             type={drawerItem.type}
+             notification={drawerItem.notification}
+             color={color}
+             activeItemColor={activeItemColor}
+           />
+          )
+        } else {
+          return <></>
+        }
+      })}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: constant.SPACING / 2,
+    justifyContent: 'space-between',
+    borderRadius: constant.borderRadius
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  label: {
+    fontSize: constant.textFontSize,
+    color: colors.dark,
+    paddingHorizontal: constant.SPACING
+  },
+  notificationBadge: {
+    paddingVertical: constant.SPACING / 5,
+    paddingHorizontal: constant.SPACING / 2,
+    borderRadius: constant.borderRadius / 2
+  },
+  view: {
+    backgroundColor: colors.white,
+    borderRadius: constant.borderRadius,
+    marginHorizontal: constant.SPACING / 2,
+    paddingVertical: 100,
+    padding: constant.SPACING / 1.5
+  }
+})
+
+export default DrawerItemList
