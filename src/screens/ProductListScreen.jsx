e0fd517939bdd5b03d632eee10288d7be833944a
Anderson Grefa <andersongrefa@gmail.com>
2023-07-30 18:45:42 -0500

    feat: creando la screen de categorias

--- src/screens/ProductListScreen.jsx
+++ src/screens/ProductListScreen.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import Constants from 'expo-constants'
+import { Text, View, StyleSheet, SectionList } from 'react-native'
+import { Card, useTheme } from 'react-native-paper'
+import { useProducts } from '../hooks/storeApi'
+import { Background, Header, Button } from '../components'
 
-const ProductListScreen = () => {
+const ProductCard = ({ item }) => {
   return (
-    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-      <Text>Productos</Text>
-    </View>
+      <Card>
+        <Card.Title title={item.title} subtitle={`$${item.price}`}/>
+        <Card.Cover source={{ uri: item.image }} />
+        <Card.Content>
+          <Text variant="titleLarge">Vendedor: {item.description}</Text>
+          <Text variant="bodyMedium">Precio: {item.price}</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button mode='contained' onPress={() => console.log('Pressed')}>Agregar al carrito</Button>
+        </Card.Actions>
+      </Card>
   )
 }
+
+const ProductListScreen = () => {
+  const { products } = useProducts()
+  const categories = products.map(product => product.category)
+  const uniqueCategories = [...new Set(categories)]
+  const data = uniqueCategories.map(category => {
+    return {
+      title: category,
+      data: products.filter(product => product.category === category)
+    }
+  })
+
+  return (
+    <Background>
+              <SectionList
+              style={styles.listContainer}
+              sections={data}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <ProductCard item={item} />}
+              ItemSeparatorComponent={() => <View style={{ height: 20 }}/>}
+              renderSectionHeader={({ section: { title } }) => (
+                <Header>{title}</Header>
+              )}/>
+    </Background>
+  )
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flexGrow: 1,
+    padding: 20,
+    width: '130%'
+  }
+})
 
 export default ProductListScreen
