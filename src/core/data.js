0d20444ffce575d01d60235e536eed74d97cf636
Anderson Grefa <andersongrefa@gmail.com>
2023-07-30 10:32:33 -0500

    feat: agregando array de datos para consumir

---
+++ src/core/data.js
@@ -0,0 +1,174 @@
+export const employees = {
+  ventas: [
+    {
+      id: 1,
+      fecha: '2023-07-25',
+      cliente: 'Juan Pérez',
+      auto: 'Toyota Corolla',
+      precio: 25000,
+      vendedor: 'María Gómez'
+    },
+    {
+      id: 2,
+      fecha: '2023-07-24',
+      cliente: 'Ana Rodríguez',
+      auto: 'Honda Civic',
+      precio: 23000,
+      vendedor: 'Pedro López'
+    },
+    {
+      id: 3,
+      fecha: '2023-07-22',
+      cliente: 'Carlos Ramírez',
+      auto: 'Ford Focus',
+      precio: 22000,
+      vendedor: 'Laura Martínez'
+    },
+    {
+      id: 4,
+      fecha: '2023-07-20',
+      cliente: 'Sofía Fernández',
+      auto: 'Nissan Sentra',
+      precio: 20000,
+      vendedor: 'Andrés González'
+    },
+    {
+      id: 5,
+      fecha: '2023-07-19',
+      cliente: 'Miguel Sánchez',
+      auto: 'Chevrolet Cruze',
+      precio: 21000,
+      vendedor: 'Elena Vargas'
+    },
+    {
+      id: 6,
+      fecha: '2023-07-18',
+      cliente: 'Luis Morales',
+      auto: 'Volkswagen Golf',
+      precio: 18000,
+      vendedor: 'Javier Herrera'
+    },
+    {
+      id: 7,
+      fecha: '2023-07-17',
+      cliente: 'María López',
+      auto: 'Hyundai Elantra',
+      precio: 19000,
+      vendedor: 'Gabriela Mendoza'
+    },
+    {
+      id: 8,
+      fecha: '2023-07-15',
+      cliente: 'José González',
+      auto: 'Kia Forte',
+      precio: 19500,
+      vendedor: 'Ricardo Gómez'
+    },
+    {
+      id: 9,
+      fecha: '2023-07-12',
+      cliente: 'Laura Torres',
+      auto: 'Mazda 3',
+      precio: 21500,
+      vendedor: 'Fernando Pérez'
+    },
+    {
+      id: 10,
+      fecha: '2023-07-10',
+      cliente: 'Fernanda Rodríguez',
+      auto: 'Chevrolet Malibu',
+      precio: 24000,
+      vendedor: 'Ana Jiménez'
+    },
+    {
+      id: 11,
+      fecha: '2023-07-09',
+      cliente: 'Carlos Ramírez',
+      auto: 'Toyota Corolla',
+      precio: 26000,
+      vendedor: 'Laura Martínez'
+    },
+    {
+      id: 12,
+      fecha: '2023-07-08',
+      cliente: 'Ana Rodríguez',
+      auto: 'Honda Civic',
+      precio: 22500,
+      vendedor: 'Pedro López'
+    },
+    {
+      id: 13,
+      fecha: '2023-07-07',
+      cliente: 'Sara Gómez',
+      auto: 'Ford Mustang',
+      precio: 35000,
+      vendedor: 'Gabriela Mendoza'
+    },
+    {
+      id: 14,
+      fecha: '2023-07-05',
+      cliente: 'Juan Pérez',
+      auto: 'Chevrolet Camaro',
+      precio: 32000,
+      vendedor: 'María Gómez'
+    },
+    {
+      id: 15,
+      fecha: '2023-07-04',
+      cliente: 'Carlos Ramírez',
+      auto: 'Ford Escape',
+      precio: 28000,
+      vendedor: 'Laura Martínez'
+    }
+  ]
+}
+
+export const shoppingCart = {
+  usuario: 'nombre_usuario',
+  productos: [
+    {
+      id: 4,
+      nombre: 'Sudadera',
+      descripcion: 'Sudadera con capucha y bolsillos',
+      precio: 28.99,
+      color: 'Gris',
+      talla: 'XL',
+      cantidad: 1,
+      imagen: 'url_de_la_imagen_4.jpg'
+    },
+    {
+      id: 5,
+      nombre: 'Falda',
+      descripcion: 'Falda plisada de cuero',
+      precio: 39.99,
+      color: 'Negro',
+      talla: 'M',
+      cantidad: 2,
+      imagen: 'url_de_la_imagen_5.jpg'
+    },
+    {
+      id: 6,
+      nombre: 'Chaqueta',
+      descripcion: 'Chaqueta acolchada con cierre',
+      precio: 55.99,
+      color: 'Azul Marino',
+      talla: 'S',
+      cantidad: 1,
+      imagen: 'url_de_la_imagen_6.jpg'
+    },
+    {
+      id: 7,
+      nombre: 'Blusa',
+      descripcion: 'Blusa con estampado floral',
+      precio: 24.99,
+      color: 'Blanco y Azul',
+      talla: 'L',
+      cantidad: 3,
+      imagen: 'url_de_la_imagen_7.jpg'
+    }
+  ],
+  subtotal: 267.93,
+  descuento: 20.00,
+  envio: 7.50,
+  total: 255.43
+}
