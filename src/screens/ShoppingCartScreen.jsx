eb8d70a15abce887742e8b5d1dea2df5e6fb380f
Anderson Grefa <andersongrefa@gmail.com>
2023-07-30 22:40:37 -0500

    feat: Agregando screen de carrito de productos

--- src/screens/ShoppingCartScreen.jsx
+++ src/screens/ShoppingCartScreen.jsx
@@ -1,13 +1,63 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import Constants from 'expo-constants'
+import { View, FlatList, StyleSheet, Image, Text } from 'react-native'
+import { Card, useTheme, Button } from 'react-native-paper'
+import { Background, BackButton } from '../components'
+import { useProducts } from '../hooks/storeApi'
 
-const HomeScreen = () => {
+const ShoppingCartCard = ({ item }) => {
+  const { colors } = useTheme()
+  const styles = makeStyles(colors)
   return (
-    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-      <Text>Carrito</Text>
-    </View>
+      <Card>
+        <Card.Title
+          title={item.title}
+          subtitle={`$${item.price}`}
+          left={() => <Image style={styles.productImage} source={{ uri: item.image }} />} />
+        <Card.Actions>
+          <Button onPress={() => console.log('Pressed')}>Quitar</Button>
+        </Card.Actions>
+      </Card>
   )
 }
 
-export default HomeScreen
+const ShoppingCartScreen = () => {
+  const { colors } = useTheme()
+  const styles = makeStyles(colors)
+  const { products } = useProducts()
+  const shoppingCart = products.filter(product => product.price < 100)
+  const totalPrice = shoppingCart.reduce((acc, product) => acc + product.price, 0)
+  return (
+    <Background>
+      <Text>Total: {totalPrice.toFixed(2)}</Text>
+      <FlatList
+      ItemSeparatorComponent={() => <View style={{ height: 10 }}/>}
+      style={styles.listContainer}
+      data={shoppingCart}
+      renderItem={({ item }) => <ShoppingCartCard item={item} />}
+      keyExtractor={item => item.id}/>
+      <View style={styles.twoColumns}>
+      <Button onPress={() => console.log('Pressed')}>Cancelar</Button>
+      <Button mode='contained' onPress={() => console.log('Pressed')}>Comprar</Button>
+      </View>
+  </Background>
+  )
+}
+
+const makeStyles = (colors) => StyleSheet.create({
+  listContainer: {
+    flexGrow: 1,
+    padding: 20,
+    width: '130%'
+  },
+  productImage: {
+    width: 50,
+    height: 60
+  },
+  twoColumns: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+export default ShoppingCartScreen
