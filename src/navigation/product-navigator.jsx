e42c023302a11f5b05665020a320c9ad6ebe149b
Anderson Grefa <andersongrefa@gmail.com>
2023-08-04 20:28:34 -0500

    feat: Agregando lazy loading en los screens de los productos

--- src/navigation/product-navigator.jsx
+++ src/navigation/product-navigator.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import { ProductListScreen, HomeScreen, ShoppingCartScreen } from '../screens'
 import CustomNavigationBar from '../components/AppBar'
+import { Text } from 'react-native-paper'
 
 const Tab = createBottomTabNavigator()
+
+const HomeComponent = lazy(() => import('../screens/HomeScreen'))
+const CategoryComponent = lazy(() => import('../screens/ProductListScreen'))
+const ShoppingCartComponent = lazy(() => import('../screens/ShoppingCartScreen'))
 
 const ProductNavigator = () => {
   return (
@@ -14,24 +18,42 @@ const ProductNavigator = () => {
         tabBarActiveTintColor: '#e91e63',
         header: (props) => <CustomNavigationBar {...props} />
       }}>
-      <Tab.Screen name="Inicio" component={HomeScreen} options={{
+      <Tab.Screen name="Inicio" options={{
         tabBarLabel: 'Inicio',
         tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
         )
-      }} />
-      <Tab.Screen name="Categoria" component={ProductListScreen} options={{
+      }} >
+        {(props) => (
+          <Suspense fallback={<Text>Cargando ....</Text>}>
+            <HomeComponent {...props} />
+          </Suspense>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Categoria" options={{
         tabBarLabel: 'Categoria',
         tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="shape-plus" color={color} size={size} />
         )
-      }}/>
-      <Tab.Screen name="Carrito" component={ShoppingCartScreen} options={{
+      }}>
+        {(props) => (
+          <Suspense fallback={<Text>Cargando ....</Text>}>
+            <CategoryComponent {...props} />
+          </Suspense>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Carrito" options={{
         tabBarLabel: 'Carrito',
         tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="cart" color={color} size={size} />
         )
-      }}/>
+      }}>
+        {(props) => (
+          <Suspense fallback={<Text>Cargando ....</Text>}>
+            <ShoppingCartComponent {...props} />
+          </Suspense>
+        )}
+      </Tab.Screen>
     </Tab.Navigator>
   )
 }
